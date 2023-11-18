c40eac770bd09c9a4eb7570187547600153cfdc6
kaustubhbhal <80494909+kaustubhbhal@users.noreply.github.com>
2023-11-17 20:59:07 -0500

    new API from visualCrossing includes historial data

--- NWSapi.py
+++ NWSapi.py
@@ -1,31 +1,15 @@
 import requests
+import sys
 
-def get_weather_forecast(latitude, longitude):
-    base_url = "https://api.weather.gov/points/{},{}".format(latitude, longitude)
+import json
+                
 
-    # Step 1: Get forecast office from the API
-    response = requests.get(base_url)
-    if response.status_code == 200:
-        data = response.json()
-        forecast_office = data['properties']['forecastOffice']
+response = requests.request("GET", "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ventura%20county%20california/2017-12-01/2017-12-31?unitGroup=us&include=days&key=GWL9ETYVUYCUBM8G78QPA4PPG&contentType=json")
+if response.status_code!=200:
+  print('Unexpected Status code: ', response.status_code)
+  sys.exit()  
 
-        # Step 2: Get the forecast from the forecast office
-        forecast_url = "{}/forecast".format(forecast_office)
-        response = requests.get(forecast_url)
-        if response.status_code == 200:
-            forecast_data = response.json()
-            return forecast_data
-        else:
-            print("Error getting forecast data. Status code:", response.status_code)
-    else:
-        print("Error getting forecast office. Status code:", response.status_code)
 
-# Example: Get the weather forecast for a specific location
-latitude = 37.7749  # Replace with your latitude
-longitude = -122.4194  # Replace with your longitude
-
-forecast = get_weather_forecast(latitude, longitude)
-if forecast:
-    # Display the forecast data
-    print("Weather Forecast for {}: {}".format(forecast['properties']['periods'][0]['name'],
-                                               forecast['properties']['periods'][0]['detailedForecast']))
+# Parse the results as JSON
+jsonData = response.json()
+        
