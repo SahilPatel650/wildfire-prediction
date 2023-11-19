714711bbfe59bae3886b917c460d7f5c7c4617d9
Mrinal Jain <2mrinaljain@gmail.com>
2023-11-19 03:48:47 -0500

    updated model inference

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from api_cache import get_elevation
 import matplotlib.pyplot as plt
 from matplotlib import colormaps
+import ai_utils
 
 if __name__ == "__main__":
     API_KEY = json.load(open('api_key.json'))['ELEVATION_API_KEY']
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     grid_data_str = sys.argv[1]
     grid_data = json.loads(grid_data_str)
     #create a 3d array
-    three_d_array = np.zeros((64, 64, 2))
+    data = np.zeros((64, 64, 2))
 
     #dump the data into json file
     with open('grid_data.json', 'w') as outfile:
@@ -28,13 +29,14 @@ if __name__ == "__main__":
             selected = arr['selected']
             print(i, j, lat, lng, selected)
             weather_data = get_elevation(lat, lng, API_KEY)
-            three_d_array[i, j, 0] = weather_data
-            three_d_array[i, j, 1] = selected
+            data[i, j, 0] = weather_data
+            data[i, j, 1] = selected
 
-    grid_data[0] = ai_utils.normalize_elevation(grid_data[0])
+    data[0] = ai_utils.normalize_elevation(data[0])
     model = ai_utils.get_model((64, 64, 2))
     model.load_weights("training/models/model.h5")
     preds = model_inference(data, model)
+    print(preds)
     plt.imshow(preds, cmap=colormaps["Reds"])
     plt.show()
     plt.save_fig("web/static/images/prediction.png", bbox_inches="tight")
